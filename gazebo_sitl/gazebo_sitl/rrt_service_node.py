b6f95189e876c20124d964e2087adeb943a00509
MDecarabas <codrea.eric@gmail.com>
2022-12-09 20:48:16 -0600

    Finished documenting rrt_service_node

--- gazebo_sitl/gazebo_sitl/rrt_service_node.py
+++ gazebo_sitl/gazebo_sitl/rrt_service_node.py
@@ -6,7 +6,14 @@ import math
 
 from nu_mavsdk_interfaces.srv import RRTService #Service to return RRT Path given start coordinates and end goal
 
-class POINT(Node):
+class POINT:
+    """
+    The POINT class provides a x and y reference point for the RRT to explore
+    
+    
+    Typical usage is POINT(x, y)
+
+    """
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -14,8 +21,8 @@ class POINT(Node):
 
 def get_random_point(x_min, x_max, y_min, y_max):
     '''
-    get_random_point takes in x & y minimum and maximum locations to generate a random point for the RRT to search a path to.
-    The function returns a call to the RRT class to search for a pathway to the points
+    get_random_point takes in x & y minimum and maximum locations to generate a random point.
+    The function returns a call to the POINT class to search for a pathway to the points
     '''
     x = random.uniform(x_min, x_max)
     y = random.uniform(y_min, y_max)
@@ -35,6 +42,10 @@ def get_nearest_node(tree, point):
     return min_node
 
 def get_new_point(nearest_node, point, step_size):
+    """
+    get_new_point takes in the nearest node, a point, and a step size. Using the step size it finds the minimum pathway to the new x/y coordinate which it
+    then returns
+    """
     dist = math.sqrt((nearest_node.x - point.x)**2 + (nearest_node.y - point.y)**2)
     if dist <= step_size:
         return point
@@ -45,12 +56,18 @@ def get_new_point(nearest_node, point, step_size):
         return Node(new_x, new_y)
 
 def check_collision(point, obstacles):
+    """
+    the check_collision function takes in the point and obstacles, checking if to get to the point it must cross through an obstacle
+    """
     for obs in obstacles:
         if point.x >= obs[0][0] and point.x <= obs[1][0] and point.y >= obs[0][1] and point.y <= obs[1][1]:
             return True
     return False
 
 def get_path(goal_node):
+    """
+    The get_path function generates a path given the node to which is aims to end up at.
+    """
     path = []
     node = goal_node
     while node:
@@ -59,6 +76,9 @@ def get_path(goal_node):
     return path
 
 def rrt(start, goal, obstacles, x_min, x_max, y_min, y_max, step_size, max_iter):
+    """
+    the rrt function returns a pathway avoiding obstacles given the size of the space, obstacles, start & end goal. 
+    """
     tree = []
     tree.append(start)
     for i in range(max_iter):
@@ -75,6 +95,11 @@ def rrt(start, goal, obstacles, x_min, x_max, y_min, y_max, step_size, max_iter)
     return None
 
 class RRT(Node):
+    """
+    The RRT class creates a service which executes the rrt path plan upon call from the drone executor node client and then returns a discretized path to the
+    goal while avoiding obstacles along the way. 
+    """
+
     def __init__(self):
         super().__init__('rrt_service')
         self.srv = self.create_service(RRTService, 'rrt_points', self.rrt_points)
@@ -85,7 +110,7 @@ class RRT(Node):
 
 
     # 7.5 x 0.2
-        obstacles = [[(1, 1), (3.5, -3.5)], [(-2.5, 6), (3.5, 6.5)], [(6, 6), (-0.5, 6.5)],  [(5.5, 13.5), (-2.5, -6.5)], [(19, 19), (3.5, -3.5)]]
+        obstacles = [[(1, 1), (3.5, -3.5)], [(-2.5, 6), (3.5, 6.5)], [(6, 6), (-0.5, 6.5)],  [(5.5, 13.5), (-2.5, -6.5)], [(19, 19), (3.5, -3.5)]] #obstacles given the created world
         x_min = -5                                                                  #minimum x to explore in 2d space
         x_max = 25                                                                  #maximum x to explore in 2d space
         y_min = -7                                                                  #minimum y to explore in 2d space
