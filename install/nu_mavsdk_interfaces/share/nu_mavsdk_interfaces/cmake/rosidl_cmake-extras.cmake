c73208885f0e38731a39e5a3546652801abbd4ec
MDecarabas <codrea.eric@gmail.com>
2022-12-09 22:27:03 -0600

    changed directory layout

--- install/nu_mavsdk_interfaces/share/nu_mavsdk_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(nu_mavsdk_interfaces_IDL_FILES "srv/RRTService.idl")
-set(nu_mavsdk_interfaces_INTERFACE_FILES "srv/RRTService.srv;srv/RRTService_Request.msg;srv/RRTService_Response.msg")
