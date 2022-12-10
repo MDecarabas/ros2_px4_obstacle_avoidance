9a736f40ad69805e133d1d49187000b2174619f5
MDecarabas <codrea.eric@gmail.com>
2022-12-09 19:23:38 -0600

    Change to make rrt better

--- gazebo_sitl/setup.py
+++ gazebo_sitl/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'gazebo_sitl'
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,8 +23,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'my_node = gazebo_sitl.drone_execute_node:main',
-            'rrt_service = gazebo_sitl.rrt_service_node:main'
+            'drone_execute_node = gazebo_sitl.drone_execute_node:main',
+            'rrt_service_node= gazebo_sitl.rrt_service_node:main'
         ],
     },
 )
