a0f8d59c2df1cc8333d0d56b1df81c861935dc17
MDecarabas <codrea.eric@gmail.com>
2022-12-09 22:35:05 -0600

    fixed yaml

--- src/gazebo_sitl/setup.py
+++ src/gazebo_sitl/setup.py
@@ -11,8 +11,10 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*'))
+        ('share/' + package_name, ['package.xml', 
+        'config/drone.yaml']),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
